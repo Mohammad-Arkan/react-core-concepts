b062243eae325aee5d7d98bd600fef8db28b4fcf
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-05-20 17:11:04 +0600

    add incease button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Device from './components/Device/Device' 
 import DeviceDetails from './components/DeviceDetails/DeviceDetails'
+import Watch from './components/watch/Watch'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,8 +12,8 @@ function App() {
   return (
     <>
     <Device name="iPhone" price="130000"></Device>
-    <Device name="Readmi-Note pro Max" price="26000"></Device>
-   
+    <Device name="Readmi-Note pro Max" price="24000"></Device>
+   <Watch></Watch>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/components/DeviceDetails/DeviceDetails.jsx
+++ src/components/DeviceDetails/DeviceDetails.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Watch from '../watch/Watch';
+
 
 const DeviceDetails = (props) => {
     console.log(props)
     return (
         <div>
             <p>Information about your Device: {props.price}</p>
+          
         </div>
     );
 };
